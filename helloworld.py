3f04597bd1978bd490649926f60dbfb4ba1c1c52
Bushflash <vot231107@gmail.com>
2026-07-15 20:22:50 +0700

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,25 @@
+# Day 1 - 30DaysOfPython Challenge
+
+print(4 + 3)             # addition(+)
+print(3 - 4)             # subtraction(-)
+print(4 * 3)             # multiplication(*)
+print(3 / 4)             # division(/)
+print(3 ** 4)            # exponential(**)
+print(3 % 4)             # modulus(%)
+print(3 // 4)            # Floor division operator(//)
+
+#name
+print("Bush")
+print("Vo")
+print("Vietnam")
+print("I am enjoying 30 days of python")
+
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
