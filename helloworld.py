c77eb311ef7e1cdfc0fb898e578f6e7a1f07c53c
Bushflash <vot231107@gmail.com>
2026-07-15 20:51:36 +0700

    3rd commit added euclid

--- helloworld.py
+++ helloworld.py
@@ -20,7 +20,15 @@ print(type(9.8))         # Float
 print(type(3.14))         # Float
 print(type(4 - 4j))      # Complex number
 print(type([1, 2, 3]))   # List
-print(type({'Asabeneh','Python',"Findland"})) # Dictionary
-print(type({"Bush"}))
-print(type({"Vo"}))
-print(type({"Vietnam"}))
+print(type({'Asabeneh','Python',"Findland"}))
+print(type("Bush"))
+print(type("Vo"))
+print(type("Vietnam"))
+print(type('Asabeneh'))  # String
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+#euclidean between (2,3) and (10,8)
+print(((2-10)**2)+((3-8)**2))
+
