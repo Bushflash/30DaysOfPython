b7385818ad22c64e6054fcbbeeb54eb9b947388f
Bushflash <vot231107@gmail.com>
2026-07-15 20:37:44 +0700

    s2test

--- helloworld.py
+++ helloworld.py
@@ -16,10 +16,11 @@ print("I am enjoying 30 days of python")
 
 # Checking data types
 print(type(10))          # Int
-print(type(3.14))        # Float
-print(type(1 + 3j))      # Complex number
-print(type('Asabeneh'))  # String
+print(type(9.8))         # Float
+print(type(3.14))         # Float
+print(type(4 - 4j))      # Complex number
 print(type([1, 2, 3]))   # List
-print(type({'name':'Asabeneh'})) # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Set
-print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type({'Asabeneh','Python',"Findland"})) # Dictionary
+print(type({"Bush"}))
+print(type({"Vo"}))
+print(type({"Vietnam"}))
